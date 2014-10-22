19e7a3c23a784c6a232f4157b0e09b1e1dda1919
Marcin Smoczynski <smoczynski.marcin@gmail.com>
2014-10-22 20:34:00 +0200

    refactoring -- use class for easier stream handling

--- sniffer.py
+++ sniffer.py
@@ -6,6 +6,27 @@ import os
 import binascii
 import time
 
+class CrossedStream(object):
+    def __init__(self, ser, other):
+        self.ser = ser
+        self.other = other
+
+    def __str__(self):
+        return self.ser.name + " <-> " + self.other.name
+
+    def name(self):
+        return self.ser.name
+
+    def process(self):
+        s = ""
+        while self.ser.inWaiting() > 0:
+            c = self.ser.read(self.ser.inWaiting())
+            if c is None or c == '': break
+            s += c
+
+        self.other.write(s)
+        return s
+
 def usage():
     sys.stderr.write("Usage: " + sys.argv[0] + " <app_pts> <serial>\n")
 
@@ -13,28 +34,23 @@ def serial_open(name):
     print "Opening " + name
     return serial.Serial(name)
 
-def data_print(s, data):
-    print "[" + time.asctime() + " | " + s.name + "]\t" + binascii.hexlify(data)
+def data_print(cr, data):
+    print "[" + time.asctime() + " | " + cr.name() + "]\t" + binascii.hexlify(data)
 
 def sniff(open_func = serial_open, print_func = data_print):
     serout = open_func(outname)
     serin = open_func(inname)
+    crout = CrossedStream(serout, serin)
+    crin = CrossedStream(serin, serout)
     fds = [serout.fileno(), serin.fileno()]
-    print "File descriptors " + " ".join([str(fd) for fd in fds])
-    serial_map = {serout.fileno(): [serout, "OUT", serin], serin.fileno(): [serin, "IN", serout]}
+
+    serial_map = {serout.fileno(): crout, serin.fileno(): crin}
 
     while True:
        readable,_,_ = select.select(fds, [], [])
        for r in readable:
-            s = ""
-            ser = serial_map[r][0]
-            while ser.inWaiting() > 0:
-                c = ser.read(ser.inWaiting())
-                if c is None or c == '': break
-                s += c
-
-            print_func(ser, s)
-            serial_map[r][2].write(s)
+           cr = serial_map[r]
+           print_func(cr, cr.process())
 
 if __name__ == "__main__":
     try:
